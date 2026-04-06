c8431f1995d4194099c43db8a19a3b4af8fb11b2
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2026-04-06 22:38:35 +0100

    Added java org.yaml.SnakeYAML parser

---
+++ parsers/java/org.yaml.snakeyaml/Main.java
@@ -0,0 +1,42 @@
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public final class Main {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("usage: yt <yaml-file> <key>");
+            System.exit(1);
+        }
+
+        var filePath = Path.of(args[0]);
+        var key = args[1];
+
+        Object loaded;
+
+        try (var input = Files.newInputStream(filePath)) {
+            var yaml = new Yaml();
+            loaded = yaml.load(input);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+            return;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(2);
+            return;
+        }
+
+        if (!(loaded instanceof Map<?, ?> map)) {
+            System.out.println("<nil>");
+            return;
+        }
+
+        var value = map.get(key);
+
+        System.out.println(value != null ? value : "<nil>");
+    }
+}
